fbeaa5c55c9f6729151182bbcd0da96987bd56d1
Sandul Munasinghe <sandultharuna6@gmail.com>
2025-06-25 17:55:37 +0530

    Implement Footer section

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -22,15 +22,15 @@ const Menu = () => {
         gsap.fromTo('.cocktail img', {
             opacity: 0,
             xPercent: -100,
-        },{
+        }, {
             xPercent: 0,
             opacity: 1,
             duration: 1,
             ease: 'power1.inOut',
         })
 
-        gsap.fromTo('.details h2', {yPercent: 100, opacity: 0}, {yPercent:0,opacity:1, ease: 'power1.inOut'})
-        gsap.fromTo('.details p', {yPercent: 100, opacity: 0}, {yPercent:0,opacity:1, ease: 'power1.inOut'})
+        gsap.fromTo('.details h2', {yPercent: 100, opacity: 0}, {yPercent: 0, opacity: 1, ease: 'power1.inOut'})
+        gsap.fromTo('.details p', {yPercent: 100, opacity: 0}, {yPercent: 0, opacity: 1, ease: 'power1.inOut'})
 
     }, [currentIndex]);
 
